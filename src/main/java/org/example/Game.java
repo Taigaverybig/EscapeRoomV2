b8460278c0f94cc3f4ca02f2205c54048e3bc54c
Hubert Szpit <hubert.szpit2005@gmail.com>
2025-06-03 00:22:13 +0200

    CheckQuestions gemaakt door tariq

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,5 +1,5 @@
 package org.example;
-
+import java.util.Scanner;
 
 
 public class Game {
@@ -15,6 +15,22 @@ public class Game {
 
         System.out.println("\nSpeler antwoordt: fout");
         checker.controleerAntwoord("fout");
+        Scanner scanner= new Scanner(System.in);
+
+        Kamer1Questions gameOn= new Kamer1Questions(); //object voor de vragen van kamer
+        CheckQuestions checker= new CheckQuestions(); //object om vragen te checken
+        gameOn.setKamer1questions(); //voegt de vragen toe aan een arraylist
+        checker.checkquestions(); //activeert de checker
+
+        for(int i=0;i<gameOn.getQuestionCount();i++) {
+            gameOn.getKamer1questions(i);
+            gameOn.SubQuestions(i);
+            String text= scanner.nextLine();
+            checker.sendCorrectionMessage(i, text);
+
+            //kopieer de bovenstaande for-loop + volgorde voor juiste vragenronde
+
         }
+
 }
 
